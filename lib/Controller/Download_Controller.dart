3df6201580956d848893bc7adf7cc0f48b20ea69
apple <fahadyousafxi>
2023-02-24 11:20:16 +0500

    commit 36

--- lib/Controller/Download_Controller.dart
+++ lib/Controller/Download_Controller.dart
@@ -1,23 +1,63 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
+import 'dart:io';
 
 
 class DownloadController {
 
-   Future<String> getDownload() async {
-    SharedPreferences pref = await SharedPreferences.getInstance();
-    var token = pref.getString('token');
-    print(token);
-    var headers = {'Authorization': 'Bearer $token'};
+  //  Future<String> getDownload() async {
+  //   SharedPreferences pref = await SharedPreferences.getInstance();
+  //   var token = pref.getString('token');
+  //   print(token);
+  //   var headers = {'Authorization': 'Bearer $token'};
+  //
+  //   // var request = http.Request('GET',
+  //   //   Uri.parse('https://pixelcare.stackbuffers.com/api/user/document/pdf',),);
+  //   //
+  //   // request.headers.addAll(headers);
+  //   //
+  //   // http.StreamedResponse response = await request.send();
+  //   //
+  //   // return '';
+  //   // // return fromJson(jsonDecode(await response.stream.bytesToString()));
+  //
+  //
+  //
+  //
+  // }
 
-    var request = http.Request('GET',
-      Uri.parse('https://pixelcare.stackbuffers.com/api/user/document/pdf',),);
 
-    request.headers.addAll(headers);
 
-    http.StreamedResponse response = await request.send();
+   Future<void> downloadPDF() async {
 
-    return request.body;
-  }
+     SharedPreferences pref = await SharedPreferences.getInstance();
+       var token = pref.getString('token');
+       print(token);
+       // var headers = {'Authorization': 'Bearer $token'};
+
+     // final url = 'https://example.com/api/get_pdf';
+     final headers = {'Authorization': 'Bearer $token'};
+     final response = await http.get(Uri.parse("https://pixelcare.stackbuffers.com/api/user/document/pdf"), headers: headers);
+
+     // Check if the response was successful
+     if (response.statusCode == 200) {
+       // Save the PDF to a file
+       final file = File('example.pdf');
+       await file.writeAsBytes(response.bodyBytes);
+       print('downloaded');
+       // print(json.response.stream.bytesToString());
+       // ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: content))
+     } else {
+       // Handle the error
+       print('Not downloaded');
+
+       throw Exception('Failed to download PDF');
+     }
+   }
+
+
 
 }
